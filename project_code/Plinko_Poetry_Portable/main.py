2ef621804bdc3798bf445481d51bdf66960b2d3d
DeqingSun <iamseer@gmail.com>
2014-02-17 22:39:47 -0500

    Add backend code
    to support uploading new contents

---
+++ project_code/Plinko_Poetry_Portable/main.py
@@ -0,0 +1,55 @@
+import webapp2
+import re
+
+from google.appengine.api import memcache
+from google.appengine.ext import ndb
+from google.appengine.ext import db
+import logging
+
+DEFAULT_CONTENT_NAME = 'default_CONTENT'
+def contents_key(contents_name=DEFAULT_CONTENT_NAME):
+    """Constructs a Datastore key for a Contents entity with contents_name."""
+    return db.Key.from_path('Plinko_contents', contents_name)
+
+class Words_content(db.Model):
+    time = db. DateTimeProperty(auto_now=True);
+    content = db.StringProperty();
+
+class Recent_data(webapp2.RequestHandler):	#TODO: add cache
+    def get(self):
+        all_data_number=16;
+        #get most recent item
+        q=Words_content.all().ancestor(contents_key(DEFAULT_CONTENT_NAME));
+        q.order("-time");
+        data=q.fetch(all_data_number)
+        self.response.headers['Content-Type'] = 'text/plain';
+        for x in range(0, all_data_number):
+            if (x<len(data)):
+                content=data[x].content;
+            else:
+                content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. ";
+            self.response.write(content);
+            self.response.write("\n");
+     
+
+class Submit_data(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain';
+        content = self.request.get('content');
+        if (re.match("^[\,\.\?\!\;\' A-Za-z0-9_-]*$", content)):
+            str_len=len(content);
+            if (str_len>=60 and str_len<=200):
+                self.response.write("OK.")
+                ct = Words_content(parent=contents_key(DEFAULT_CONTENT_NAME));	#put into database
+                ct.content = content;
+                ct.put()				
+            else:
+                self.response.write("Your " + str(str_len) + " characters context isn't between 60 to 200 characters")
+        else:
+            self.response.write("Special characters are not allowed");
+
+
+application = webapp2.WSGIApplication([
+    ('/submit', Submit_data),
+    ('/recent', Recent_data),
+], debug=True)
