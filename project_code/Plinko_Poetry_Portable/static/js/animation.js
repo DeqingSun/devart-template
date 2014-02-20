9f39f129d12ea37dcbcd1869a91d5f1407ad1252
DeqingSun <dsun@September.local>
2014-02-20 17:12:03 -0500

    Add positive animation (not finished yet)

---
+++ project_code/Plinko_Poetry_Portable/static/js/animation.js
@@ -0,0 +1,67 @@
+// JavaScript Document
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+var posi_frames=100;
+var frame_ptr=0;
+var mw_dist_x_pos=[];
+function draw_positive_animation(ptr){
+	var fade_out_frm=10;var move_frm=40;var move_down_frm=70;
+	if (ptr < fade_out_frm){
+		var color_value=Math.round(255*ptr/(fade_out_frm-1));
+		for (var i=0;i<8;i++){
+			ctx.beginPath();
+			ctx.rect(0, text_pos_y[i], blackout_l[i], -text_size_px);
+			ctx.fillStyle = 'rgb('+color_value+','+color_value+','+color_value+');';
+			ctx.fill();
+			ctx.rect(blackout_r[i], text_pos_y[i], w-blackout_r[i], -text_size_px);
+			ctx.fill();	
+		}
+	}else if(ptr < (move_down_frm)){
+		ctx.font = text_size_px+"px Arial";	//test main text
+		if (ptr==fade_out_frm){	//calculate something in 1st frame
+			var widest = 0;
+			for (var i=0;i<8;i++){
+				var wid=ctx.measureText(words_selected[i]).width;
+				if (wid>widest) widest = wid;
+			}
+			for (var i=0;i<8;i++){
+				mw_dist_x_pos[i]=w*0.99-widest;
+			}
+			
+		}
+		ctx.beginPath();
+		ctx.rect(0, text_pos_y[7]+text_size_px*0.1, w, text_pos_y[0]-text_size_px*(1.2)-text_pos_y[7]);
+		ctx.fillStyle = 'white';
+		ctx.fill();
+		ctx.textBaseline="bottom"; 
+		ctx.fillStyle = 'black';
+		if (ptr < (move_frm))
+			for (var i=0;i<8;i++){
+				//ctx.fillText(words_selected[i],words_selected_pos[i],text_pos_y[i]);
+				ctx.fillText(words_selected[i],map_range(ptr,fade_out_frm,move_frm-1,words_selected_pos[i],mw_dist_x_pos[i]),text_pos_y[i]);	
+			}
+		else{
+			for (var i=0;i<8;i++){
+				ctx.fillText(words_selected[i],mw_dist_x_pos[i],text_pos_y[i]+(h)*(ptr-move_frm)/(move_down_frm-move_frm));
+				
+			}
+		}
+		
+		
+	}else{
+		var alpha_all=(255*(ptr-move_frm)/(posi_frames-move_frm))
+		ctx.beginPath();
+		ctx.rect(0, text_pos_y[7]+text_size_px*0.1, w, text_pos_y[0]-text_size_px*(1.2)-text_pos_y[7]);
+		ctx.fillStyle = 'rgba(255,255,255,'+alpha_all+')';
+		ctx.fill();
+	}
+	
+	if (ptr>posi_frames){
+		return false;	
+	}else{
+		return true;	
+	}
+}
