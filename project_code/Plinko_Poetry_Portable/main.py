a5d117e730e1db1505d8fb4e52d896c3958fccc9
DeqingSun <dsun@September.local>
2014-03-14 12:43:11 -0400

    Clean obsolete functions

--- project_code/Plinko_Poetry_Portable/main.py
+++ project_code/Plinko_Poetry_Portable/main.py
@@ -62,10 +62,6 @@ def base64_modify_back(base64data):
     base64data = base64data+('='*padding_len);
     return base64data;
 
-class Words_content(db.Model):
-    time = db. DateTimeProperty(auto_now=True);
-    content = db.StringProperty();
-
 class Poems_content(db.Model):
     time = db. DateTimeProperty(auto_now=True);
     content = db.StringProperty();
@@ -92,54 +88,6 @@ class Recent_data(webapp2.RequestHandler):
                 content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. ";
             self.response.write(content);
             self.response.write("\n");
-
-class Recent_Poems(webapp2.RequestHandler):	#TODO: add cache
-    def get(self):
-        all_data_number=10;
-        #get most recent item     
-        q=Poems_content.all().ancestor(contents_key(POEM_CONTENT_NAME));
-        q.order("-time");
-        data=q.fetch(all_data_number)
-        self.response.headers['Content-Type'] = 'text/plain';
-        for i in range(0, len(data)):
-            self.response.write(data[i].comment + "\t" + data[i].content);
-            self.response.write("\n");
-     
-
-class Submit_data(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain';
-        content = self.request.get('content');
-        if (re.match("^[\,\.\?\!\;\' A-Za-z0-9_-]*$", content)):
-            str_len=len(content);
-            if (str_len>=60 and str_len<=200):
-                self.response.write("OK.")
-                ct = Words_content(parent=contents_key(DEFAULT_CONTENT_NAME));	#put into database
-                ct.content = content;
-                ct.put();
-                generate_recent_memcache(16);
-            else:
-                self.response.write("Your " + str(str_len) + " characters context isn't between 60 to 200 characters")
-        else:
-            self.response.write("Special characters are not allowed");
-            
-class New_Poem(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain';
-        poem = self.request.get('poem');
-        if (re.match("^[\,\.\?\!\;\' A-Za-z0-9_-]*$", poem)):
-            comment = self.request.get('comment');
-            if (comment=="G"):
-                comment="GOOD";
-            if (comment=="B"):
-                comment="BAD";
-            self.response.write("A "+comment+" poem is:"+poem);
-            ct = Poems_content(parent=contents_key(POEM_CONTENT_NAME));	#put into database
-            ct.content = poem;
-            ct.comment = comment;
-            ct.put()
-        else:
-            self.response.write("Special characters are not allowed");
 
 class Fetch_data(webapp2.RequestHandler):   
     def get(self):
@@ -212,10 +160,7 @@ class Gallery(webapp2.RequestHandler):
         self.response.out.write("</body></html>")
 
 application = webapp2.WSGIApplication([
-    ('/submit', Submit_data),
     ('/recent', Recent_data),
-    ('/new_poem', New_Poem),
-    ('/recent_poems', Recent_Poems),
     ('/fetch_data', Fetch_data),
     ('/upload_poster', Upload_poster),
     ('/get_poster', Get_poster),
